8b17ddf2c33b13d6d406d9ceda2c5f63fc00d6a4
tukaianirban <mukherjee.anirban86@gmail.com>
2024-12-26 23:22:52 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center">
+        Coming Soon
+      </div>
+    </>
+  );
+}
+
+export default App;
